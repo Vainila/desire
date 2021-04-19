c8bd219ee324dc8648fea5f387a585de7a31d309
Vainila <workmail1421@gmail.com>
2021-04-19 13:14:22 +0300

    add: desire index page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
-let a = document.querySelectorAll("div");
-a+="kdfjkdj";
-//ksjdfklfsjflkjs
-let b = 3;
+
+
+$(function (){
+  $('.header__btn').on('click', function (){
+    console.log("click");
+    $('.rightside-menu').removeClass('rightside-menu--close')
+  });
+
+  $('.rightside-menu__close').on('click', function (){
+    $('.rightside-menu').addClass('rightside-menu--close')
+  });
+  $('.top__slider').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true
+  });
+})
